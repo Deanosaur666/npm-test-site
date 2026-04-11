c70e4d2a2dccb43a55ec4f8ec49f8d01674865a0
Dean <deanyockey@yahoo.com>
2026-04-10 20:24:18 -0700

    Fetches JSON from github

--- src/main.js
+++ src/main.js
@@ -2,9 +2,65 @@ import Graph from "graphology";
 import Sigma from "sigma";
 import { Octokit, App } from "octokit";
 
-const graph = new Graph();
-graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "red" });
-graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "green" });
-graph.addEdge("1", "2", { size: 5, color: "black" });
+// get graph JSON from:
+// https://github.com/Deanosaur666/npm-test-site/blob/graph.json
+// using github API
+
+const octokit = new Octokit();
+
+async function loadGraph() {
+
+    // API request
+    let result = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: 'Deanosaur666',
+        repo: 'npm-test-site',
+        path: 'graph.json',
+    })
+
+    // Decode Base64 content to text
+    let decodedContent = atob(result.data.content);
+    
+    // Parse the text as JSON
+    let graphData = JSON.parse(decodedContent);
+    
+    return graphData;
+    
+}
+
+let graph_json = await loadGraph()
+
+console.log("Graph: ")
+console.log(graph_json)
+
+let graph = new Graph();
+
+let edges = []
+
+for (let i = 0; i < graph_json.length; i++) {
+    let node = graph_json[i];
+    graph.addNode(node.id, node);
+    for (let j = 0; j < node.edges.length; j ++) {
+        let target = node.edges[j]
+        let edge = [node.id, target]
+        console.log(edge)
+        edges.push([node.id, target])
+    }
+}
+
+for (let i = 0; i < edges.length; i ++) {
+    graph.addEdge(edges[i][0], edges[i][1])
+}
 
 const renderer = new Sigma(graph, document.getElementById("container"));
+
+function addNodeButton() {
+
+}
+
+function removeNodeButton() {
+
+}
+
+function saveButton() {
+
+}
